fca817f10e1fea7e91da6b4c6d44b02df834f47c
Sneha Sharma <sneha.workmail1998@gmail.com>
2019-09-29 19:48:36 +0530

    Update LinearRegressionTest.py

--- LinearRegressionTest.py
+++ LinearRegressionTest.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 
-data=pd.read_csv(r"C:\Users\Sneha Sharma\Downloads\CarPriceCoding.csv")
+data=pd.read_csv(r"C:\Users\Sneha Sharma\Downloads\CarPriceCoding.csv")        #insert your csv file address inside ""
 
-curbweight=data['curbweight']
-price=data['price']
+curbweight=data['curbweight']                                                  #list containing curbweights
+price=data['price']                                                            #listcontaining prices
 
 
 def mean(list):
@@ -19,7 +19,7 @@ def mean(list):
 x_mean=mean(curbweight)
 y_mean=mean(price)
 
-def covariance(x,y):
+def covariance(x,y):                    # x and y are list
     x_error=[]
     y_error=[]
     xy_error=[]
@@ -51,7 +51,7 @@ def intercept(x,y):
     c=y_mean-(x_mean*slope(x,y))
     return c
 
-def predict_y(x,y,x_predict):
+def predict_y(x,y,x_predict):                           #x predict is value of x(curbweight) using which we will find the y value(i.e the price value)
     y_predict=(slope(x,y)*x_predict)+intercept(x,y)
     return y_predict
 
